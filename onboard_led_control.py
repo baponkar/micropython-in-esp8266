14dab1a6dab269c1ed6b037da11579b3b25f2d2c
Bapon Kar <baponkar600@gmail.com>
2021-01-26 13:01:16 +0530

    Update onboard_led_control.py

--- onboard_led_control.py
+++ onboard_led_control.py
@@ -1,7 +1,12 @@
+#Onbard led of nodemcu esp8266 board blink
+#Writer : Bapon Kar
+#Last Update : 26/01/2021
+
+
 from machine import Pin
 import time
 
-led = Pin(2, Pin.OUT) #Define onboard pin , external led should connect to D4 pin of esp8266 board
+led = Pin(2, Pin.OUT) #Define onboard pin , external led should connect to D4 pin of esp8266 board through 1Kohom resistance
 
 #led.low()
  
